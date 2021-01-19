848425eb752526da4d3971ef314995f22f2df137
hemanto <mehemanto@gmail.com>
2021-01-19 15:19:22 +0530

    add cat section

--- src/Layouts/Catgy.js
+++ src/Layouts/Catgy.js
@@ -4,9 +4,11 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import CatCard from '../Components/CatCard';
 import { Diet } from '../Config'
+import { useSelector, useDispatch } from "react-redux";
 
 const Catgy = () => {
     const [catrecips, setCatrecips] = useState([])
+    const [localdata, setLocaldata] = useState(JSON.parse(localStorage.getItem('Random_Recipes')) || [])
     const get = (diettile) => {
         axios.get(`https://api.spoonacular.com/recipes/complexSearch?diet=${diettile}&apiKey=6f57734196684cecb7ec58a0c69a76c7&number=20&includeNutrition=true`)
             .then(function (response) {
@@ -35,11 +37,36 @@ const Catgy = () => {
     //         })
 
     // }
-    // useEffect(() => {
-    //     get("vegetarian")
-    // }, [])
 
 
+    const state = useSelector(state => state.xyz)
+
+    // const data = localdata.filter(item => {
+    //     if (item.vegetarian) {
+    //         localStorage.setItem("vegetarian", JSON.stringify([item]));
+    //         console.log(item);
+    //     } else if (item.vegan) {
+    //         localStorage.setItem("vegan", JSON.stringify([item]));
+    //     } else if (item.glutenFree) {
+    //         localStorage.setItem("glutenFree", JSON.stringify([item]));
+    //     } else {
+    //         localStorage.setItem("All", JSON.stringify([item]));
+    //     }
+    // })
+
+    const vergit = localdata.filter(item => item.vegetarian)
+    console.log(vergit);
+    useEffect(() => {
+        console.log(localdata);
+        const data = localdata.map(item => {
+            if (item.vegetarian) {
+                localStorage.setItem("vegetarian", JSON.stringify([item]));
+            } else {
+                localStorage.setItem("Non-vegetarian", JSON.stringify([item]));
+            }
+        })
+
+    }, [])
 
     return (
         <div>
